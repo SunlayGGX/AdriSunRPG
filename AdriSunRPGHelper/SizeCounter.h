39a6714250c97587f9a001fedb5d749568fae081
SunlayGGX <sunlay.g@free.fr>
2017-07-30 17:43:02 -0400

    Size counter meta object

---
+++ AdriSunRPGHelper/SizeCounter.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace AdriSunRPG
+{
+    /*
+    Meta object to count the size of all elements.
+    */
+    template<class FirstType, class ... OtherType> 
+    struct SizeCounter
+    {
+    public:
+        enum
+        {
+            value = sizeof(FirstType) + SizeCounter<OtherType...>::value;
+        };
+    };
+
+    /*
+    Version with one element
+    */
+    template<class LonelyType>
+    struct SizeCounter<LonelyType>
+    {
+    public:
+        enum
+        {
+            value = sizeof(LonelyType);
+        };
+    };
+}
