176ddc32efb3433e95f7e60eca9e82359e3c682d
SunlayGGX <sunlay.g@free.fr>
2017-07-30 22:41:05 -0400

    init, update in Game loop and destroy of RenderEngine

--- AdriSunRPG/GlobalEngine.cpp
+++ AdriSunRPG/GlobalEngine.cpp
@@ -1,5 +1,7 @@
 #include "GlobalEngine.h"
 
+#include "RenderEngine.h"
+
 #include "GameConfig.h"
 
 using namespace AdriSunRPG;
@@ -18,13 +20,14 @@ GlobalEngine::~GlobalEngine()
 
 void GlobalEngine::initialize()
 {
-
+    RenderEngine::instance().initialize();
 }
 
 void GlobalEngine::destroy()
 {
     this->quit();
 
+    RenderEngine::instance().destroy();
 }
 
 void GlobalEngine::run()
@@ -32,6 +35,8 @@ void GlobalEngine::run()
     while(m_run)
     {
         /*TODO : Update the Game Loop*/
+
+        RenderEngine::instance().update();
 
         //Bad, but until we have a proper TimeManager, this will do the job...
         //TODO : Call a proper method from a Time Manager or Synchronize with VSync from RenderEngine swap chain present (present1 method) or so
